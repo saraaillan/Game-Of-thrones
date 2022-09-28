b15feba96705397804e41b583bfd593470ad02c1
saraaillan <saraillan9@gmail.com>
2022-09-28 20:29:19 +0200

    commit inicial

---
+++ js/script.js
@@ -0,0 +1,14 @@
+
+let b=7;
+let a=8;
+
+const cuadrado=(base)=>{
+     return base*base;
+}
+console.log(cuadrado(7))
+
+
+let num=parseInt(promp("intro num"))
+if(!isNaN(num)&&nun>10){
+    console.log(potencia(num))
+}
